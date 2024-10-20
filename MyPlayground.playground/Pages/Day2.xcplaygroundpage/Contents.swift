0492c26191e2d3333845b3f33621f5734bc92d36
Nathan Foo <ntfoo27@outlook.com>
2024-10-19 19:39:25 -0500

    Day 2

--- MyPlayground.playground/Pages/Day2.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Day2.xcplaygroundpage/Contents.swift
@@ -4,6 +4,29 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+var isAuthenticated = true // boolean value
+isAuthenticated = false
+
+print(!isAuthenticated)
+
+isAuthenticated.toggle() // flip boolean value
+
+print(isAuthenticated)
+
+let name = "Joe"
+let age = 18
+
+// String concatenation and interpolation
+let message = "Hello world! My name is \(name) and my age is " + String(age)
+print(message)
+
+let calc = "5 x 5 is \(5 * 5)"
+print(calc)
+
+// celsius to fahrenheit
+
+let celsius = 25
+let fahrenheit = celsius * 9 / 5 + 32
+print("\(celsius) degrees celsius in fahrenheit is \(fahrenheit)")
 
 //: [Next](@next)
